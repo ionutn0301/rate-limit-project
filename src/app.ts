2640f21116e46813b27973102cff517ce8b33afd
Ionut N <ionutn0301@gmail.com>
2025-05-28 21:52:39 +0300

    Enhance project setup with updated dependencies, improved .gitignore, and added ESLint and Prettier configurations. Introduced dotenv for environment variable management and refined TypeScript settings for better module handling.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,17 @@
+import dotenv from 'dotenv';
 import express, { Request, Response } from 'express';
 
 const app = express();
+dotenv.config();
+
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-    res.json({ message: 'Welcome to the Express TypeScript API!' });
+  res.json({ message: 'Welcome to the Express TypeScript API!' });
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-}); 
+  console.log(`Server is running on port ${port}`);
+});
