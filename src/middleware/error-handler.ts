022dafde0217c8ebc7adbe9f0cfc995e8d8d7361
Ionut N <ionutn0301@gmail.com>
2025-05-29 22:42:01 +0300

    fix: update error handler to include NextFunction

--- src/middleware/error-handler.ts
+++ src/middleware/error-handler.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 export class AppError extends Error {
   constructor(
@@ -11,7 +11,13 @@ export class AppError extends Error {
   }
 }
 
-export const errorHandler = (err: Error | AppError, _req: Request, res: Response): Response => {
+export const errorHandler = (
+  err: Error | AppError,
+  _req: Request,
+  res: Response,
+  // eslint-disable-next-line no-unused-vars
+  _next: NextFunction,
+): Response => {
   if (err instanceof AppError) {
     return res.status(err.statusCode).json({
       error: err.message,
